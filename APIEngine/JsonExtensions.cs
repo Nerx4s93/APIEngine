775025ad6f7c932fc329850b141b95b2f4bac4ca
Nerx <nikitahysnutdinov@gmail.com>
2026-05-11 19:08:14 +0700

    класс для удобного парсинга json

---
+++ APIEngine/JsonExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace APIEngine;
+
+public static class JsonExtensions
+{
+    public static JsonElement GetByPathOrThrow(this JsonElement element, string path)
+    {
+        var current = element;
+
+        foreach (var part in path.Split('.'))
+        {
+            if (!current.TryGetProperty(part, out current))
+            {
+                throw new KeyNotFoundException($"Path not found: {path}");
+            }
+        }
+
+        return current;
+    }
+
+    public static bool TryGetByPath(this JsonElement element, string path, out JsonElement result)
+    {
+        var current = element;
+
+        foreach (var part in path.Split('.'))
+        {
+            if (!current.TryGetProperty(part, out current))
+            {
+                result = default;
+                return false;
+            }
+        }
+
+        result = current;
+        return true;
+    }
+}
